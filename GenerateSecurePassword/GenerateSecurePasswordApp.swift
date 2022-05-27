d4c407f9d1537469476a7486f4d55bc7c7b9f854
onggiahuy97 <ogh.huy@gmail.com>
2022-05-27 08:32:35 -0700

    Initial Commit

---
+++ GenerateSecurePassword/GenerateSecurePasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateSecurePasswordApp.swift
+//  GenerateSecurePassword
+//
+//  Created by Huy Ong on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct GenerateSecurePasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
